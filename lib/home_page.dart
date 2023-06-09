9301f99e53fa4ee2cde9283c4062e21a70294dfa
Vivek Patil <vivekptl892@gmail.com>
2023-06-09 14:56:57 +0530

    Created initial screen UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -16,19 +20,48 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Sample Text'),
-          ],
-        ),
+      body: Column(
+        children: [
+          const Expanded(
+              child: Row(
+            children: [
+              Expanded(child: ReusableCard(color: activeCardColor)),
+              Expanded(child: ReusableCard(color: activeCardColor)),
+            ],
+          )),
+          const Expanded(child: ReusableCard(color: activeCardColor)),
+          const Expanded(
+              child: Row(
+            children: [
+              Expanded(child: ReusableCard(color: activeCardColor)),
+              Expanded(child: ReusableCard(color: activeCardColor)),
+            ],
+          )),
+          Container(
+            color: bottomContainerColor,
+            margin: const EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({super.key, required this.color, this.cardChild});
+
+  final Color color;
+  final Widget? cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(10.0)),
+      child: cardChild,
     );
   }
 }
