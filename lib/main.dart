4a0f63ace4313d84b297af083aabb23bedb511e8
Vivek Patil <patilvivekw@gmail.com>
2023-08-18 16:21:30 +0530

    Minot update to test gitlab access.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Update',
       theme: ThemeData(
           colorScheme: ColorScheme.fromSwatch().copyWith(
             primary: const Color(0xFF0A0E21),
